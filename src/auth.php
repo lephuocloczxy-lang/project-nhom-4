275891592d98f05753120acb53aeef0dc49b9000
vinhkhiem <khiemdangvinh8@gmail.com>
2025-10-29 22:43:20 +0700

    Delete src directory

--- src/auth.php
+++
@@ -1,8 +0,0 @@
-<?php
-if (session_status() === PHP_SESSION_NONE) {
-    session_start();
-}
-if (!isset($_SESSION['user_id'])) {
-    header("Location: /bai01_quanly_sv/public/index.php?action=login");
-    exit;
-}
